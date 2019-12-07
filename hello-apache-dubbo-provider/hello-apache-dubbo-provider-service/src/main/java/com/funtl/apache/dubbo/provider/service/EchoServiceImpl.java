11d07c6af9801f520883c541249f82d6aa1e9b1b
liaowen <liaowen2013win@163.com>
2019-12-07 17:51:12 +0800

    设置Dubbo负载均衡策略

--- hello-apache-dubbo-provider/hello-apache-dubbo-provider-service/src/main/java/com/funtl/apache/dubbo/provider/service/EchoServiceImpl.java
+++ hello-apache-dubbo-provider/hello-apache-dubbo-provider-service/src/main/java/com/funtl/apache/dubbo/provider/service/EchoServiceImpl.java
@@ -6,6 +6,7 @@
 package com.funtl.apache.dubbo.provider.service;
 
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.funtl.apache.dubbo.provider.api.EchoService;
 
@@ -17,8 +18,12 @@ import com.funtl.apache.dubbo.provider.api.EchoService;
  */
 @Service(version = "1.0.0")
 public class EchoServiceImpl implements EchoService {
+
+    @Value("${dubbo.protocol.port}")
+    private String port;
+
     @Override
     public String echo(String string) {
-        return "Echo Hello Dubbo " + string;
+        return "Echo Hello Dubbo " + string + ", i am from port: " + port;
     }
 }
