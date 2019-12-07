ab38cd3fbc3348cad145b4334a8efa2fc589e98a
liaowen <liaowen2013win@163.com>
2019-12-07 18:12:23 +0800

    设置Dubbo外部化配置

--- hello-apache-dubbo-consumer/src/main/java/com/funtl/apache/dubbo/consumer/controller/EchoController.java
+++ hello-apache-dubbo-consumer/src/main/java/com/funtl/apache/dubbo/consumer/controller/EchoController.java
@@ -6,6 +6,8 @@
 package com.funtl.apache.dubbo.consumer.controller;
 
 import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +18,20 @@ import com.funtl.apache.dubbo.provider.api.EchoService;
  * @Author: L.Wen
  * @created_at: 2019/12/06 17:37
  */
+//配置动态刷新
+@RefreshScope
 @RestController
 public class EchoController {
 
     @Reference(version = "1.0.0")
     private EchoService echoService;
 
+    @Value("${user.name}")
+    private String username;
+
+
     @GetMapping(value = "/echo/{string}")
     public String echo(@PathVariable String string) {
-        return echoService.echo(string);
+        return echoService.echo(string) + ", " + username;
     }
 }
